e1baf753e74d7396946d4089834e8c29a3ffd90d
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2025-10-23 11:08:06 +0200

    fix: startup (#8)
    * fix: add wait and retry for initial connection to pipe client
    
    * fix: serialisation and deserialisation
    
    * feat: read logs with async function and only keep error logs
    
    * feat: remove log

--- src/FanControl.Liquidctl/LiquidctlBridgeWrapper.cs
+++ src/FanControl.Liquidctl/LiquidctlBridgeWrapper.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Diagnostics;
 using System.IO.Pipes;
 using System.Text;
@@ -9,28 +8,28 @@ namespace FanControl.LiquidCtl
 {
     public class PipeRequest
     {
-        [JsonPropertyName("command")]
+        [JsonProperty("command")]
         public required string Command { get; set; }
 
-        [JsonPropertyName("data")]
+        [JsonProperty("data")]
         public FixedSpeedRequest? Data { get; set; }
     }
 
     public class SpeedKwargs
     {
-        [JsonPropertyName("channel")]
+        [JsonProperty("channel")]
         public required string Channel { get; set; }
 
-        [JsonPropertyName("duty")]
+        [JsonProperty("duty")]
         public required int Duty { get; set; }
     }
 
     public class FixedSpeedRequest
     {
-        [JsonPropertyName("device_id")]
+        [JsonProperty("device_id")]
         public required int DeviceId { get; set; }
 
-        [JsonPropertyName("speed_kwargs")]
+        [JsonProperty("speed_kwargs")]
         public required SpeedKwargs SpeedKwargs { get; set; }
     }
 
@@ -56,30 +55,41 @@ namespace FanControl.LiquidCtl
                         StartInfo = new ProcessStartInfo
                         {
                             FileName = liquidctlexe,
+                            Arguments = "--log-level ERROR",
                             UseShellExecute = false,
                             CreateNoWindow = true,
                             RedirectStandardOutput = true,
                             RedirectStandardError = true
                         }
                     };
-                    bridgeProcess.OutputDataReceived += (sender, args) =>
-                    {
-                        if (!string.IsNullOrEmpty(args.Data))
-                        {
-                            logger.Log($"[FanControl.LiquidCtl] {args.Data}");
-                        }
-                    };
-                    bridgeProcess.ErrorDataReceived += (sender, args) =>
-                    {
-                        if (!string.IsNullOrEmpty(args.Data))
-                        {
-                            logger.Log($"[FanControl.LiquidCtl] {args.Data}");
-                        }
-                    };
+
                     _ = bridgeProcess.Start();
-                    bridgeProcess.BeginOutputReadLine();
-                    bridgeProcess.BeginErrorReadLine();
+
+                    // Read streams asynchronously to avoid deadlock
+                    _ = Task.Run(() => ReadStreamAsync(bridgeProcess.StandardOutput, isError: false));
+                    _ = Task.Run(() => ReadStreamAsync(bridgeProcess.StandardError, isError: true));
+
+                    Thread.Sleep(2000);
                 }
+            }
+        }
+
+        private async Task ReadStreamAsync(StreamReader reader, bool isError)
+        {
+            try
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        logger.Log($"[FanControl.LiquidCtl] {line}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Expected when stream is disposed
             }
         }
 
@@ -159,7 +169,6 @@ namespace FanControl.LiquidCtl
 
                 try
                 {
-                    logger.Log($"Sending command: {request.Command}");
                     string requestJson = JsonConvert.SerializeObject(request);
                     byte[] requestData = Encoding.UTF8.GetBytes(requestJson);
 
@@ -168,7 +177,6 @@ namespace FanControl.LiquidCtl
 
                     byte[] buffer = new byte[4096];
                     int bytesRead = _pipeClient.Read(buffer, 0, buffer.Length);
-                    logger.Log($"Received {bytesRead} bytes from pipe");
                     return Encoding.UTF8.GetString(buffer, 0, bytesRead);
                 }
                 catch (IOException ex)
